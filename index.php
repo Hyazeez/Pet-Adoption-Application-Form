ff099e9f38e3349e21cd3b417689a10bb9019044
Azeez shaban <hyazeez123@gmail.com>
2025-05-03 22:54:06 +0530

    Update images

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pet Adoption Application Form</title>
     <link rel="stylesheet" href="stlye.css">
-    <link rel="icon" href="/Pet-Adoption-Application-Form/SVG/paw.png" type="image/png">
+    <link rel="icon" href="/SVG/paw.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     <style>
         input {
@@ -22,7 +22,7 @@
         }
 
         body {
-            background-image: url('/Pet-Adoption-Application-Form/images/pet_with_family.jpg');
+            background-image: url('/images/pet_with_family.jpg');
             background-size: cover;
             background-position: center;
             background-attachment: fixed;
